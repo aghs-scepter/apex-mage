8742da6ae0007a7d3c3c7f43a2bdad4f32013ac6
aghs <111622257+aghs-scepter@users.noreply.github.com>
2025-03-02 16:37:20 -0500

    Feature: Starting or restarting the service can now use cache for faster boot times

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ if [ ! "$(docker ps -q -f name=apex-mage)" ]; then
         # Remove the existing container
         docker rm apex-mage
     else
-        docker build --no-cache -t apex-mage .
+        docker build -t apex-mage .
         # Run a new container with the SQLite DB directory mounted and environment variables loaded
         docker run -d --name apex-mage --restart unless-stopped --env-file /app/.env -v /appdata:/app/data apex-mage
     fi
@@ -17,7 +17,7 @@ else
     echo "Application 'apex-mage' is already running. Stopping and recreating it..."
     docker stop apex-mage
     docker rm apex-mage
-    docker build --no-cache -t apex-mage .
+    docker build -t apex-mage .
     docker run -d --name apex-mage --restart unless-stopped --env-file /app/.env -v /appdata:/app/data apex-mage
 fi
 
